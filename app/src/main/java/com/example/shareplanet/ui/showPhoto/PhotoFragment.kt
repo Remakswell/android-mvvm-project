34a867cd436857263408400ad119a75c88a95d23
mshymko <maksym.shymko@geeksforless.com>
2021-04-21 12:32:22 +0300

    added request permission

--- app/src/main/java/com/example/shareplanet/ui/showPhoto/PhotoFragment.kt
+++ app/src/main/java/com/example/shareplanet/ui/showPhoto/PhotoFragment.kt
@@ -1,18 +1,21 @@
 package com.example.shareplanet.ui.showPhoto
 
+import android.Manifest
 import android.app.WallpaperManager
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.*
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.davemorrissey.labs.subscaleview.ImageSource
 import com.example.shareplanet.R
-import com.example.shareplanet.ui.MainActivity
 import com.nostra13.universalimageloader.core.ImageLoader
 import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener
 import kotlinx.android.synthetic.main.fragment_show_photo.*
@@ -22,7 +25,15 @@ class PhotoFragment : Fragment() {
 
     private val args: PhotoFragmentArgs by navArgs()
     private var photo: Bitmap? = null
-    private lateinit var mainActivity: MainActivity
+
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                performSharing()
+            } else {
+                showStorageWarning()
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,8 +46,6 @@ class PhotoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
 
-        mainActivity = activity as MainActivity
-
         loadImage()
     }
 
@@ -47,18 +56,18 @@ class PhotoFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_set_wallpaper -> {
-                if (photo == null) {
-                    showLoadingWarning()
-                } else {
+                if (photo != null) {
                     setWallpaperBitmap()
+                } else {
+                    showLoadingWarning()
                 }
                 return true
             }
             R.id.action_share -> {
-                if (photo == null) {
-                    showLoadingWarning()
-                } else {
+                if (photo != null) {
                     performSharing()
+                } else {
+                    showLoadingWarning()
                 }
                 return true
             }
@@ -69,7 +78,7 @@ class PhotoFragment : Fragment() {
     private fun loadImage() {
         ImageLoader.getInstance().loadImage(args.photoUrl, object : SimpleImageLoadingListener() {
             override fun onLoadingComplete(imageUri: String, view: View?, loadedImage: Bitmap) {
-                if (!activity?.isFinishing()!!) {
+                if (!activity?.isFinishing!!) {
                     photo = loadedImage
                     nasaImage.setImage(ImageSource.cachedBitmap(loadedImage))
                     progress.visibility = View.GONE
@@ -80,22 +89,32 @@ class PhotoFragment : Fragment() {
 
     private fun setWallpaperBitmap() {
         val wallpaperManager =
-            WallpaperManager.getInstance(mainActivity.applicationContext)
+            WallpaperManager.getInstance(activity?.applicationContext)
         wallpaperManager.setBitmap(photo)
         Toast.makeText(context, R.string.set_as_wallpaper_completed, Toast.LENGTH_SHORT).show()
     }
 
     private fun performSharing() {
-        val path = MediaStore.Images.Media.insertImage(
-            mainActivity.contentResolver, photo, args.photoUrl, "")
-        val uri = Uri.parse(path)
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.type = "image/jpeg"
-        intent.putExtra(Intent.EXTRA_STREAM, uri)
-        startActivity(Intent.createChooser(intent, getString(R.string.share)))
+        if (ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            val path = MediaStore.Images.Media.insertImage(
+                activity?.contentResolver, photo, args.photoUrl, ""
+            )
+            val uri = Uri.parse(path)
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "image/jpeg"
+            intent.putExtra(Intent.EXTRA_STREAM, uri)
+            startActivity(Intent.createChooser(intent, getString(R.string.share)))
+        } else {
+            requestPermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
     }
 
     private fun showLoadingWarning() {
         Toast.makeText(context, getString(R.string.photo_is_loading), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showStorageWarning() {
+        Toast.makeText(context, getString(R.string.permission_denied), Toast.LENGTH_SHORT).show()
     }
 }
